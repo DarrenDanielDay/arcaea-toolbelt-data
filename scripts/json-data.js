4d72232dc5f1f43aeefb64e10b5090b43ba92641
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2023-11-24 00:34:13 +0800

    feat: add list data

--- scripts/json-data.js
+++ scripts/json-data.js
@@ -1,8 +1,12 @@
-import { mkdir, readFile, readdir, stat, writeFile } from "fs/promises";
+// @ts-check
+import { copyFile, mkdir, readFile, readdir, stat, writeFile } from "fs/promises";
 import { createHmac } from "crypto";
-import { resolve } from "path";
-import { cwd } from "process";
+import { parse, resolve } from "path";
+import { argv, cwd } from "process";
 
+/**
+ * @param {string} filename
+ */
 async function hashFile(filename) {
   const hmac = createHmac("sha256", "arcaea-toolbelt-data");
   const text = await readFile(filename, { encoding: "utf-8" });
@@ -12,6 +16,9 @@ async function hashFile(filename) {
 const root = cwd();
 const dataDir = resolve(root, "src", "data");
 
+/**
+ * @param {string} path
+ */
 async function readJSON(path) {
   const content = await readFile(path, { encoding: "utf-8" });
   return JSON.parse(content);
@@ -19,15 +26,30 @@ async function readJSON(path) {
 
 async function generateIndex() {
   const meta = "meta.json";
+  const metaFile = resolve(dataDir, meta);
+  /** @type {import('@arcaea-toolbelt/models/misc').ArcaeaToolbeltMeta} */
+  const metadata = await readJSON(metaFile);
+  const version = metadata.version;
+  // metadata -> version -> songlist/packlist
+  const copySubPaths = [
+    `../arcaea/arcaea_${version}/assets/songs/songlist`,
+    `../arcaea/arcaea_${version}/assets/songs/packlist`,
+  ];
+  await Promise.all(
+    copySubPaths.map(async (path) => {
+      const url = new URL(path, import.meta.url);
+      await copyFile(url, resolve(dataDir, `${parse(path).base}.json`));
+    })
+  );
   const files = (await readdir(dataDir)).filter((file) => file !== meta);
   const index = await Promise.all(
     files.map(async (file) => {
       const hash = await hashFile(resolve(dataDir, file));
-      return { file, hash: hash.slice(0, 8) };
+      /** @type {import('@arcaea-toolbelt/models/misc').ArcaeaToolbeltMeta['index'][number]} */
+      const item = { file, hash: hash.slice(0, 8) };
+      return item;
     })
   );
-  const metaFile = resolve(dataDir, meta);
-  const metadata = await readJSON(metaFile);
   metadata.index = index;
   await writeFile(metaFile, JSON.stringify(metadata, undefined, 2), {
     encoding: "utf-8",
@@ -55,7 +77,9 @@ async function buildStatic() {
 }
 
 async function main() {
-  await generateIndex();
+  if (argv.includes("--generate")) {
+    await generateIndex();
+  }
   await buildStatic();
 }
 
