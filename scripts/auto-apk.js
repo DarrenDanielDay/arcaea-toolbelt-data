1bedf4be0128a9cd7e4c42f811c2525ddc6e2b22
DarrenDanielDay <Darren_Daniel_Day@hotmail.com>
2025-07-27 02:58:55 +0800

    update: `v6.7.1`

--- scripts/auto-apk.js
+++ scripts/auto-apk.js
@@ -5,9 +5,13 @@ import { extractArcaeaAPK, copyKeyFiles, apkPaths } from "./arcaea.js";
 import { generateAssetsInfo } from "./generate-assets-info.js";
 import { buildStatic, generateIndex } from "./json-data.js";
 import { downloadFileWithNode } from "./utils.js";
+import { rm } from "fs/promises";
 
 const apkInfo = await getLatestVersion();
 const { apkURL, extractURL } = apkPaths(apkInfo.version);
+if (process.argv.includes("--force")) {
+  await rm(apkURL, { force: true, recursive: true });
+}
 if (!existsSync(apkURL)) {
   await downloadFileWithNode(apkInfo.url, apkURL);
 }
